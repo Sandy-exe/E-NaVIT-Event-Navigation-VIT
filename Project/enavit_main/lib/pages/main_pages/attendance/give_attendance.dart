52706d4a24946fb9e1ca60f2b053af50c361237b
thananjayan26 <jayanselva26@gmail.com>
2024-10-02 12:50:17 +0530

    BLE attempt

--- Project/enavit_main/lib/pages/main_pages/attendance/give_attendance.dart
+++ Project/enavit_main/lib/pages/main_pages/attendance/give_attendance.dart
@@ -2,8 +2,10 @@ import 'dart:developer';
 
 import 'package:enavit_main/services/services.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+// import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
 
 class GiveAttendance extends StatefulWidget {
   const GiveAttendance({super.key});
@@ -13,166 +15,220 @@ class GiveAttendance extends StatefulWidget {
 }
 
 class _GiveAttendanceState extends State<GiveAttendance> {
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  Barcode? result;
-  QRViewController? controller;
-  bool isPermissionGranted = false;
+  // final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  // Barcode? result;
+  // QRViewController? controller;
+  // bool isPermissionGranted = false;
+  // FlutterBluePlus flutterBlue = FlutterBluePlus.instance;
+  BluetoothDevice? teacherDevice;
+  final String studentId = '21BCE0666'; // Unique ID for each student
 
   @override
   void initState() {
     super.initState();
-    _checkCameraPermission();
+    startAdvertising();
   }
 
-  @override
-  void reassemble() {
-    super.reassemble();
-    if (controller != null) {
-      controller!.pauseCamera();
-      controller!.resumeCamera();
+  void startAdvertising() {
+    FlutterBluePlus.startScan(timeout: Duration(seconds: 10));
+
+    FlutterBluePlus.scanResults.listen((results) {
+      for (ScanResult r in results) {
+        if (r.device.name == 'TeacherDevice') {
+          setState(() {
+            teacherDevice = r.device;
+          });
+        }
+      }
+    });
+  }
+
+  void sendAttendance() async {
+    if (teacherDevice != null) {
+      await teacherDevice!.connect();
+      List<BluetoothService> services = await teacherDevice!.discoverServices();
+      for (BluetoothService service in services) {
+        for (BluetoothCharacteristic characteristic
+            in service.characteristics) {
+          if (characteristic.properties.write) {
+            await characteristic.write(studentId.codeUnits);
+            await teacherDevice!.disconnect();
+          }
+        }
+      }
     }
-  }
-
-  Future<void> _checkCameraPermission() async {
-    var status = await Permission.camera.status;
-    if (status.isDenied) {
-      status = await Permission.camera.request();
-    }
-
-    if (status.isGranted) {
-      setState(() {
-        isPermissionGranted = true;
-      });
-    } else if (status.isPermanentlyDenied) {
-      openAppSettings();
-    } else {
-      setState(() {
-        isPermissionGranted = false;
-      });
-    }
-  }
-
-  Future<void> _stopCamera() async {
-    await controller?.pauseCamera();
-    controller?.dispose();
-  }
-
-  Future<void> _showCheckingAttendanceMessage(String? scannedData) async {
-    // Show loading dialog
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) => const AlertDialog(
-        title: Text('Checking Attendance'),
-        content: Row(
-          children: [
-            CircularProgressIndicator(),
-            SizedBox(width: 20),
-            Text('Please wait...'),
-          ],
-        ),
-      ),
-    );
-
-    // Simulate checking attendance
-    Services service = Services();
-    String response = await service.checkAttendance(scannedData!);
-
-    print('Attendance check response: $response');
-
-    // Dismiss loading dialog
-    Navigator.of(context).pop();
-
-    // Show result message
-    if (response == 'success') {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Attendance checked successfully!')),
-      );
-    } else if (response == 'Not a participant') {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-            content: Text('Not a Participant of the correct Event!')),
-      );
-    } else if (response == 'Already attended') {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Attendance already checked!')),
-      );
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Failed to check attendance!')),
-      );
-    }
-  }
-
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!isPermissionGranted) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('QR Code Scanner'),
-        ),
-        body: const Center(
-          child:
-              Text('Camera permission denied. Please enable it in settings.'),
-        ),
-      );
-    }
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('QR Code Scanner'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 4,
-            child: QRView(
-              key: qrKey,
-              onQRViewCreated: _onQRViewCreated,
-              overlay: QrScannerOverlayShape(
-                borderColor: Colors.red,
-                borderRadius: 10,
-                borderLength: 30,
-                borderWidth: 10,
-                cutOutSize: 300,
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Center(
-              child: (result != null)
-                  ? Text('QR Code Data: ${result!.code}')
-                  : const Text('Scan a QR code'),
-            ),
-          ),
-        ],
+      appBar: AppBar(title: Text('Student - Attendance Sender')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: sendAttendance,
+          child: Text('Send Attendance'),
+        ),
       ),
     );
   }
-
-  void _onQRViewCreated(QRViewController controller) {
-    setState(() {
-      this.controller = controller;
-    });
-
-    controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData;
-      });
-      _handleScan(scanData);
-    });
-  }
-
-  void _handleScan(Barcode scanData) {
-    print('Scanned QR Code Data: ${scanData.code}');
-    _stopCamera();
-    _showCheckingAttendanceMessage(scanData.code);
-  }
 }
+
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _checkCameraPermission();
+//   }
+
+//   @override
+//   void reassemble() {
+//     super.reassemble();
+//     if (controller != null) {
+//       controller!.pauseCamera();
+//       controller!.resumeCamera();
+//     }
+//   }
+
+//   Future<void> _checkCameraPermission() async {
+//     var status = await Permission.camera.status;
+//     if (status.isDenied) {
+//       status = await Permission.camera.request();
+//     }
+
+//     if (status.isGranted) {
+//       setState(() {
+//         isPermissionGranted = true;
+//       });
+//     } else if (status.isPermanentlyDenied) {
+//       openAppSettings();
+//     } else {
+//       setState(() {
+//         isPermissionGranted = false;
+//       });
+//     }
+//   }
+
+//   Future<void> _stopCamera() async {
+//     await controller?.pauseCamera();
+//     controller?.dispose();
+//   }
+
+//   Future<void> _showCheckingAttendanceMessage(String? scannedData) async {
+//     // Show loading dialog
+//     showDialog(
+//       context: context,
+//       barrierDismissible: false,
+//       builder: (context) => const AlertDialog(
+//         title: Text('Checking Attendance'),
+//         content: Row(
+//           children: [
+//             CircularProgressIndicator(),
+//             SizedBox(width: 20),
+//             Text('Please wait...'),
+//           ],
+//         ),
+//       ),
+//     );
+
+//     // Simulate checking attendance
+//     Services service = Services();
+//     String response = await service.checkAttendance(scannedData!);
+
+//     print('Attendance check response: $response');
+
+//     // Dismiss loading dialog
+//     Navigator.of(context).pop();
+
+//     // Show result message
+//     if (response == 'success') {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         const SnackBar(content: Text('Attendance checked successfully!')),
+//       );
+//     } else if (response == 'Not a participant') {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         const SnackBar(
+//             content: Text('Not a Participant of the correct Event!')),
+//       );
+//     } else if (response == 'Already attended') {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         const SnackBar(content: Text('Attendance already checked!')),
+//       );
+//     } else {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//         const SnackBar(content: Text('Failed to check attendance!')),
+//       );
+//     }
+//   }
+
+//   @override
+//   void dispose() {
+//     controller?.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     if (!isPermissionGranted) {
+//       return Scaffold(
+//         appBar: AppBar(
+//           title: const Text('QR Code Scanner'),
+//         ),
+//         body: const Center(
+//           child:
+//               Text('Camera permission denied. Please enable it in settings.'),
+//         ),
+//       );
+//     }
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('QR Code Scanner'),
+//       ),
+//       body: Column(
+//         children: <Widget>[
+//           Expanded(
+//             flex: 4,
+//             child: QRView(
+//               key: qrKey,
+//               onQRViewCreated: _onQRViewCreated,
+//               overlay: QrScannerOverlayShape(
+//                 borderColor: Colors.red,
+//                 borderRadius: 10,
+//                 borderLength: 30,
+//                 borderWidth: 10,
+//                 cutOutSize: 300,
+//               ),
+//             ),
+//           ),
+//           Expanded(
+//             flex: 1,
+//             child: Center(
+//               child: (result != null)
+//                   ? Text('QR Code Data: ${result!.code}')
+//                   : const Text('Scan a QR code'),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+
+//   void _onQRViewCreated(QRViewController controller) {
+//     setState(() {
+//       this.controller = controller;
+//     });
+
+//     controller.scannedDataStream.listen((scanData) {
+//       setState(() {
+//         result = scanData;
+//       });
+//       _handleScan(scanData);
+//     });
+//   }
+
+//   void _handleScan(Barcode scanData) {
+//     print('Scanned QR Code Data: ${scanData.code}');
+//     _stopCamera();
+//     _showCheckingAttendanceMessage(scanData.code);
+//   }
+// }
--- Project/enavit_main/lib/pages/main_pages/attendance/take_attendance.dart
+++ Project/enavit_main/lib/pages/main_pages/attendance/take_attendance.dart
@@ -1,45 +1,113 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
-class TakeAttendance extends StatelessWidget {
-  final String qrData;
-  final String? embeddedImagePath;
+
+class TakeAttendance extends StatefulWidget {
+  // final String qrData;
+  // final String? embeddedImagePath;
 
   const TakeAttendance({
     super.key,
-    required this.qrData,
-    this.embeddedImagePath,
+    // required this.qrData,
+    // this.embeddedImagePath,
   });
+
+  @override
+  _TakeAttendanceState createState() => _TakeAttendanceState();
+}
+
+class _TakeAttendanceState extends State<TakeAttendance> {
+  // FlutterBluePlus flutterBlue = FlutterBluePlus;
+  List<ScanResult> scannedDevices = [];
+
+  @override
+  void initState() {
+    super.initState();
+    scanForDevices();
+  }
+
+  void scanForDevices() {
+    FlutterBluePlus.startScan(timeout: Duration(seconds: 10));
+
+    FlutterBluePlus.scanResults.listen((results) {
+      setState(() {
+        scannedDevices = results;
+      });
+    });
+
+    FlutterBluePlus.stopScan();
+  }
+
+  void connectToDevice(BluetoothDevice device) async {
+    await device.connect();
+    List<BluetoothService> services = await device.discoverServices();
+
+    for (BluetoothService service in services) {
+      for (BluetoothCharacteristic characteristic in service.characteristics) {
+        if (characteristic.properties.read) {
+          var attendanceToken = await characteristic.read();
+          logAttendance(attendanceToken);
+          await device.disconnect();
+        }
+      }
+    }
+  }
+
+  void logAttendance(List<int> token) {
+    String tokenString = String.fromCharCodes(token);
+    print('Received attendance token: $tokenString');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('QR Code'),
-      ),
-      body: Center(
-        child: QrImageView(
-          data: qrData,
-          version: QrVersions.auto,
-          size: 320,
-          gapless: false,
-          embeddedImage:
-              embeddedImagePath != null ? AssetImage(embeddedImagePath!) : null,
-          embeddedImageStyle: embeddedImagePath != null
-              ? const QrEmbeddedImageStyle(
-                  size: Size(80, 80),
-                )
-              : null,
-          errorStateBuilder: (context, error) {
-            return const Center(
-              child: Text(
-                'Uh oh! Something went wrong...',
-                textAlign: TextAlign.center,
-              ),
-            );
-          },
-        ),
+      appBar: AppBar(title: Text('Taking attendance')),
+      body: ListView.builder(
+        itemCount: scannedDevices.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(scannedDevices[index].device.name),
+            subtitle: Text(scannedDevices[index].device.id.toString()),
+            onTap: () => connectToDevice(scannedDevices[index].device),
+          );
+        },
       ),
     );
   }
 }
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('QR Code'),
+//       ),
+//       body: Center(
+//         child: QrImageView(
+//           data: widget.qrData,
+//           version: QrVersions.auto,
+//           size: 320,
+//           gapless: false,
+//           embeddedImage: widget.embeddedImagePath != null
+//               ? AssetImage(widget.embeddedImagePath!)
+//               : null,
+//           embeddedImageStyle: widget.embeddedImagePath != null
+//               ? const QrEmbeddedImageStyle(
+//                   size: Size(80, 80),
+//                 )
+//               : null,
+//           errorStateBuilder: (context, error) {
+//             return const Center(
+//               child: Text(
+//                 'Uh oh! Something went wrong...',
+//                 textAlign: TextAlign.center,
+//               ),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
